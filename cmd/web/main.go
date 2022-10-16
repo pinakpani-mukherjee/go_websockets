c4f48cfe5bc5ccacab85f41121f47f52f535b55c
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-10-16 20:28:37 +0530

    added websocket endpoint

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Hello World!!!")
+	mux := routes()
+
+	log.Println("Starting web server on port 8080.")
+
+	_ = http.ListenAndServe(":8080", mux)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"cmd/web/main.go/internal/handlers"
+
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndPoint))
+
+	return mux
+}
