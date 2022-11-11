abe123fcda82b97033707c63450cfae05ed37f67
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-11-11 09:00:31 +0530

    finished basic messenger

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -116,7 +116,13 @@ func ListenToWsChannel() {
 			users := getUserList()
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
+			broadcastToAll(response)
 		}
+
 		// response.Action = "Got here"
 		// response.Message = fmt.Sprintf("Some Message, and action was %s", e.Action)
 		// broadcastToAll(response)
